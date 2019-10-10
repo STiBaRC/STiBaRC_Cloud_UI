35ab37a5693acf9e578b2453fa9de07db9570eb8
Ben Border <ben@bunnbuns.net>
2019-10-10 17:34:21 -0400

    e

--- js/project.js
+++ js/project.js
@@ -12,28 +12,7 @@ if(projects[projectId].active){
         statusTag.innerHTML = '<span class="red dc" title="Not Active"><i class="material-icons">&#xe160;</i> </span><span class="red">Not Active</span>';
 }
 
-//Credit to:
-//codepen.io/carolynvelez/pen/MawmVb
-window.addEventListener("load", function() {
-	// store tabs variable
-	var myTabs = document.querySelectorAll("ul.nav-tabs > li");
-  function myTabClicks(tabClickEvent) {
-		for (var i = 0; i < myTabs.length; i++) {
-			myTabs[i].classList.remove("active");
-		}
-		var clickedTab = tabClickEvent.currentTarget;
-		clickedTab.classList.add("active");
-		tabClickEvent.preventDefault();
-		var myContentPanes = document.querySelectorAll(".tab-pane");
-		for (i = 0; i < myContentPanes.length; i++) {
-			myContentPanes[i].classList.remove("active");
-		}
-		var anchorReference = tabClickEvent.target;
-		var activePaneId = anchorReference.getAttribute("href");
-		var activePane = document.querySelector(activePaneId);
-		activePane.classList.add("active");
-	}
-	for (i = 0; i < myTabs.length; i++) {
-		myTabs[i].addEventListener("click", myTabClicks)
-	}
-});
+tabLinks = document.querySelectorAll('a[id^="tab-"]');
+for (i = 1; i < tabLinks.length+1; i++) {
+    document.getElementById("tab-"+i).href = 'project.html?id='+projectId+'&tab='+i;
+}
