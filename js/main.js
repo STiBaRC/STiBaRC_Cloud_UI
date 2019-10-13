046ed59f84cb91a38bf1d452e95fc07c866980f2
Ben Border <ben@bunnbuns.net>
2019-10-13 14:37:51 -0400

    added new project button and error for no projects

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-var projectsLength = Object.keys(projects).length;
+var projectsLength = 0;
+projectsLength = Object.keys(projects).length;
+var myProjects = document.getElementById("myProjects");
+if(projectsLength == 0){
+    myProjects.innerHTML = '<br><center><h2>No Projects</h2></center>';
+}
 for (var i = 1; i < projectsLength+1; i++) {
     var projectId = "p0"+i;
-    var myProjects = document.getElementById("myProjects");
     var statusTag = null;
     if(projects[projectId].active){
         var statusTag = '<span class="badge green">Active</span>';
     }else{
         var statusTag = '<span class="badge red">Not Active</span>';
     }
-    
+
     myProjects.innerHTML += '<a class="page-item" href="project.html?id='+projectId+'"> <b>'+projects[projectId].name+'</b> '+statusTag+' </a>';
 }
